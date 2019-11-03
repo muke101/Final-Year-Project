ace2d1ebe4c868d12bc3ba30d9c65abc6795d1b7
muke <muke101@protonmail.com>
2019-11-03 21:08:11 +0000

    changed how limits are transformed

--- montecarlo.c
+++ montecarlo.c
@@ -3,31 +3,38 @@
 #include <stdlib.h>
 #include <time.h>
 
-double nfEquation(double x, double k, double z, double phi)	{
-	double fc = z*pow(1+2*pow((1-z)/z,0.5)*tan(k)*cos(phi)+pow(tan(k),2)*((1-z)/z),(1-x/2))+(1-z)*pow(1-2*pow(z/(1-z),0.5)*tan(k)*cos(phi)+(z*pow(tan(k),2)/(1-z)), (1-x/2));
-	double Hq = 1-((z*(1-z))/(1+pow(tan(k),2)))*pow(2*cos(phi)+tan(k)*(1-2*z)/pow(z*(1-z),0.5), 2);
-	return (log(fc)*Hq)/(4*M_PI);
-}
-
 double uniform(double a, double b)	{
 	double range = b-a;
 	return a + (rand()*range)/RAND_MAX;
+}
+
+double nfEquation(double x, double k, double z, double phi_k)	{
+
+	while (k == 1)
+		k = uniform(0, 1);
+	double u = pow(k/(1-k), 0.5);
+	double u2 = pow(u,2);
+	double phi = phi_k*2*M_PI;
+
+	double fc = z*pow(1+2*pow((1-z)/z,0.5)*u*cos(phi)+((1-z)/z)*u2, 1-x/2)+(1-z)*pow(1-2*pow(z/(1-z),0.5)*u*cos(phi)+(z/(1-z))*u2, 1-x/2); 
+	double Hq = 1-((z*(1-z))/(1+u2))*pow(2*cos(phi)+(1-2*z)*u/pow(z*(1-z),0.5),2);
+
+	return (1/(u2*(1+u2)))*(1/(4*M_PI))*Hq*log(fc);
 }
 
 double monteCarlo(double (*equation)(double, double, double, double), double x, unsigned long long N)	{
 	double I = 0;
 	double V = pow(M_PI, 2);
 	unsigned long long i;
-	double phi, k, z;	
+	double phi_k, k, z;	
 
 	srand(time(0)); //seed RNG
 
 	for (i=0; i < N; ++i)	{
-		phi = uniform(0, 2*M_PI);
+		phi_k = uniform(0, 1);
 		z = uniform(0, 1);
-		while ((k=uniform(0, M_PI/2)) == M_PI/2)
-			k = uniform(0, M_PI/2);
-		I += equation(x, k, z, phi);
+		k = uniform(0, 1);
+		I += equation(x, k, z, phi_k);
 	}
 
 	return (V*I)/N;
