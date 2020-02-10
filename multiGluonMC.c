6b0b99d4c3b1cdf0df29627b3ed22bd781380071
muke <muke101@protonmail.com>
2020-02-10 14:43:20 +0000

    completed basic multi-gluon monte carlo procedure

--- multiGluonMC.c
+++ multiGluonMC.c
@@ -20,18 +20,10 @@ double uniform(double a, double b)  {
    return a + (rand()*range)/RAND_MAX;
 }
 
-unsigned fact(unsigned n, unsigned t)	{
-	if (n == 1)	{
-		return t;	
-	}
-	return fact(n-1,t*n);	
-}
-
 unsigned iZeta(double zetaPrev, double epsi, double R, double *res, unsigned n)	{
-	double zeta = zetaPrev*pow(uniform(epsi,1),1./R);	
+	double zeta = zetaPrev*pow(uniform(0,1),1./R);	
 	if (zeta > epsi)	{
-		res[0] *= 1./zeta; //this quickly sky rockets to infinity
-		res[1] += zeta;
+		*res += zeta;
 		return iZeta(zeta, epsi, R, res, n+1);	
 	}
 	else	{
@@ -39,40 +31,39 @@ unsigned iZeta(double zetaPrev, double epsi, double R, double *res, unsigned n)	
 	}
 }
 
+double stddev;
+
 double monteCarlo(double epsi, double R, unsigned long long N)	{
 	double currentProductSum, r, I = 0, I2 = 0;
-	double res[2];
+	double res;
 	unsigned long long i;
 	unsigned n;
 	srand(time(0)); //seed RNG
 
-	res[0] = 1; //holds product sum of 1/zeta_i
 	for (i=0; i < N; ++i)	{
-		currentProductSum = res[0];
-		res[1] = 0; //holds sum of zeta_i
-		n = iZeta(1, epsi, R, res, 0);
-		if (res[1] < 1 && n > 0)	{
-			r = pow(R,n)*res[0]*(1-res[1]/N)/fact(n,1);
-			I+=r;
-			I2+=pow(r,2);
-		}
-		else	{
-			res[0] = currentProductSum; //undo invalidated changes to product sum
+		res = 0; //holds sum of zeta_i
+		n = iZeta(1, epsi, R, &res, 0);
+		if (res < 1 && n > 0)	{
+			I+=1./N;
+			I2+=1./N;
 		}
 	}
 
-	return pow(epsi,R)*I;
+	double mu = I/N;
+	stddev = pow((1./N)*(I2-pow(I,2)), 0.5);
+
+	return I;
 }
 
 int main(int argc, char **argv)	{
 	int i;
 	size_t x = 20;
-	unsigned long long N = 10000;
-	double epsi = 10e-3;
+	unsigned long long N = 1000000;
+	double epsi = 1e-40;
 	double *R = arange(0.1,2,x);
 	
 	for(i=0; i <= x; i++)	{
-		printf("%f\n",monteCarlo(epsi, R[i], N));	
+		printf("R: %f, I: %f, std: %f\n",R[i],monteCarlo(epsi, R[i], N),stddev);	
 	}
 
 	return 0;
